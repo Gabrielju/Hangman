4707bc199167075e0e7e6dda5603d45ba2796421
Gabrielju <gabrieljulien0804@gmail.com>
2020-11-03 11:21:32 +0100

    ajout1

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Hangman
+
+creation of a hanged man with html and js language and bootstrap
+for my web dev training (the programming language was not specified)
+[Lien](https://gabrielju.github.io/Hangman/index.html).
+
+## Development comments
+development is to spend just some worry to explain to the different var to coexist
+
+## possible improvement
+
+the hanged man is still basic we could of course give several types of word (abject, animals, flowers ...) and functions like changing the background color in case of a good or bad answer.
---
+++ js/hangman.js
@@ -0,0 +1,98 @@
+var color_var = [ //var of words
+
+    "green",
+    "red",
+    "yellow",
+    "blue",
+    "pink",
+    "orange",
+    "purple",
+    "brown",
+    "black",
+    "gray"
+]
+
+let answer = '';
+let maxWrong = 6; // maw value
+let mistakes = 0; // count of mis
+let guessed = [];
+let wordStatus = null; //wors
+
+function randomWord() {
+  answer = color_var[Math.floor(Math.random() * color_var.length)];
+}
+
+function generateButtons() {
+  let buttonsHTML = 'abcdefghijklmnopqrstuvwxyz'.split('').map(letter =>
+    `
+      <button
+        class="btn btn-lg btn-secondary m-3
+        "
+        id='` + letter + `'
+        onClick="handleGuess('` + letter + `')"
+      >
+        ` + letter + `
+      </button>
+    `).join('');
+
+  document.getElementById('keyboard').innerHTML = buttonsHTML;
+}
+
+function handleGuess(chosenLetter) {
+  guessed.indexOf(chosenLetter) === -1 ? guessed.push(chosenLetter) : null;
+  document.getElementById(chosenLetter).setAttribute('disabled', true);
+
+  if (answer.indexOf(chosenLetter) >= 0) {
+    guessedWord();
+    checkIfGameWon();
+  } else if (answer.indexOf(chosenLetter) === -1) { // incrementation of miss
+    mistakes++;
+    updateMistakes();
+    checkIfGameLost();
+    updateHangmanPicture();
+  }
+}
+
+function updateHangmanPicture() {
+  document.getElementById('hangmanPic').src = './images/' + mistakes + '.png';//modification du png
+}
+
+function checkIfGameWon() {
+  if (wordStatus === answer) {
+    document.getElementById('keyboard').innerHTML = 'You Won!!!';
+  }
+}
+
+function checkIfGameLost() { //validator of lost
+  if (mistakes === maxWrong) {
+    document.getElementById('wordSpotlight').innerHTML = 'The answer was: ' + answer;
+    document.getElementById('keyboard').innerHTML = 'You Lost!!!';
+  }
+}
+
+function guessedWord() {
+  wordStatus = answer.split('').map(letter => (guessed.indexOf(letter) >= 0 ? letter : " _ ")).join('');//statue of words by defaut
+
+  document.getElementById('wordSpotlight').innerHTML = wordStatus;
+}
+
+function updateMistakes() { //update the miss factor
+  document.getElementById('mistakes').innerHTML = mistakes;
+}
+
+function reset() {//fonction reset
+  mistakes = 0;
+  guessed = [];
+  document.getElementById('hangmanPic').src = './images/0.png';
+
+  randomWord();
+  guessedWord();
+  updateMistakes();
+  generateButtons();
+}
+
+document.getElementById('maxWrong').innerHTML = maxWrong;
+
+randomWord();
+generateButtons();
+guessedWord();
